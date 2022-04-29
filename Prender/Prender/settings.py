83ee90cfdf57a947879991a87908b1f72d4262db
CamiloFloresDuoc <cm.flores@duocuc.cl>
2022-04-28 22:25:01 -0400

    inclusion bootstrap jquery y index style

--- Prender/Prender/settings.py
+++ Prender/Prender/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #terceros
+    'bootstrap4',
+    'crispy_forms', 
+    #django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #mis apps
     'core',
 ]
 
